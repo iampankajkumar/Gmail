bf6f5592cff696a6fffc453517fe06eff50c13d2
Pankaj Kumar <pankaj.kumar@skilrock.com>
2018-04-25 11:34:39 +0530

    commmit

---
+++ src/main/java/com/pankaj/dto/Gmail/App.java
@@ -0,0 +1,13 @@
+package com.pankaj.dto.Gmail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
